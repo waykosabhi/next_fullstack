d84dbe6867ac9a2ca35fb628ea878431ce7fc3b6
waykosabhi <102040576+waykosabhi@users.noreply.github.com>
2023-09-13 10:00:46 +0530

    first commit

--- components/TodoTable.tsx
+++ components/TodoTable.tsx
@@ -33,7 +33,7 @@ const TodoTable = () => {
   return (
     <>
       {allTodos.map((item) => (
-        <div className="flex gap-5">
+        <div className="flex gap-5" key={item._id}>
           <h1>{item.task}</h1>
           <button>DELETE</button>
         </div>
